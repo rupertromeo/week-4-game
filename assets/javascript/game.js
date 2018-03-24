fdebb47fc92a3f92ca7ec93c34e2bfaaa685664c
Manuel Romo <rupertromeo86@gmail.com>
2018-03-24 09:51:15 -0700

    Updated index.html file and gameplay within game.js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,5 +3,43 @@
 
 
 */
+$(document).ready(function() {
 
-var targetNumber = 137
+    //This is our target number that we want to reach by clicking portals
+    const target = 137;
+
+    //This is the current score we get from clicking crystals
+    const currentScore = 0;
+
+    //These are our gameplay variables
+    const wins = 0;
+    const losses = 0;
+    const portals;
+
+    //Here we get our random numbers to assign to the portals
+    function portalNum() {
+        return {
+            portalOne: {
+                points: Math.floor(Math.random() * 10) + 1,
+                imageURL: "assets/images/RM_Portal.gif",
+            },
+            portalTwo: {
+                points: Math.floor(Math.random() * 137) + 100,
+                imageURL: "assets/images/RM_Portal.gif",
+            },
+            portalThree: {
+                points: Math.floor(Math.random() * 13) + 1,
+                imageURL: "assets/images/RM_Portal.gif",
+            }
+        };
+    }
+
+    //Here we reset our game
+    function reset() {
+        //set current score total to 0
+        currentScore = 0;
+        //Give portals new random number
+        portals = portalNum();
+    }
+
+});
