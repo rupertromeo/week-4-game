092c0707e6193df2f12c3bd706afb0d482cf0dd6
Manuel Romo <rupertromeo86@gmail.com>
2018-03-27 19:51:34 -0700

    Updated gameplay functions to show some display items such as portals and messages to show player depending on wins or losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -41,5 +41,46 @@ $(document).ready(function() {
         //Give portals new random number
         portals = portalNum();
     }
+    //Here we update the page with how the game is going
+    function update(winner) {
+        $("#status-area").empty();
+
+        if (winner === true) {
+            $("#status-area")
+            .append($("<p>")
+            .text("Look at that Morty. We made it back home. Let's go get some of that McDonald's Mulan Schezuan Sauce!"));
+            reset();
+            showCurrentScore();
+        }
+
+        else if (winner === false) {
+            $("#status-area")
+            .append($("<p>")
+            .text("Oh no, Morty!!! There's no coming back from this Morty."));
+            reset();
+            showCurrentScore();
+        }
+        
+        //Our display for our wins and losses
+        let winSpan = $("<span>"). text(wins);
+        let lossSpan = $("<span>").text(losses);
+
+        let winsText = $("<p>").text("Wins: ");
+        let lossesText = $("<p>").text("Losses: ");
+
+        winsText.append(winSpan);
+        lossesText.append(lossSpan);
+    }
+    //Add our portals to the page
+    //We use a for loop because we need to loop through our 3 portals
+    function showPortals() {
+        for (let key in portals) {
+            const eachPortal = $("<div class='portal-button' data-name='" + key + "'>");
+            const portalImg = $("<img alt='image' class='portal-img'>")
+            .attr("src", portals[key].imageURL);
+            eachPortal.append(portalImg);
+            $("#portalContainer").append(eachPortal);
+        }
+    }
 
 });
