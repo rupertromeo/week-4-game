085645259f993839b4331a58b42e36811e350728
Manuel Romo <rupertromeo86@gmail.com>
2018-03-27 21:20:56 -0700

    Updated gameplay functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,27 +9,27 @@ $(document).ready(function() {
     const target = 137;
 
     //This is the current score we get from clicking crystals
-    const currentScore = 0;
+    let currentScore = 0;
 
     //These are our gameplay variables
-    const wins = 0;
-    const losses = 0;
-    const portals;
+    let wins = 0;
+    let losses = 0;
+    let portals;
 
     //Here we get our random numbers to assign to the portals
     function portalNum() {
         return {
             portalOne: {
                 points: Math.floor(Math.random() * 10) + 1,
-                imageURL: "assets/images/RM_Portal.gif",
+                imageURL: "assets/images/RM_Portal.jpg",
             },
             portalTwo: {
                 points: Math.floor(Math.random() * 137) + 100,
-                imageURL: "assets/images/RM_Portal.gif",
+                imageURL: "assets/images/RM_Portal.jpg",
             },
             portalThree: {
                 points: Math.floor(Math.random() * 13) + 1,
-                imageURL: "assets/images/RM_Portal.gif",
+                imageURL: "assets/images/RM_Portal.jpg",
             }
         };
     }
@@ -70,17 +70,55 @@ $(document).ready(function() {
 
         winsText.append(winSpan);
         lossesText.append(lossSpan);
+
+        $("#winloss-area").append(winsText);
+        $("#winloss-area").append(lossesText);
     }
     //Add our portals to the page
     //We use a for loop because we need to loop through our 3 portals
     function showPortals() {
         for (let key in portals) {
-            const eachPortal = $("<div class='portal-button' data-name='" + key + "'>");
-            const portalImg = $("<img alt='image' class='portal-img'>")
+            let eachPortal = $("<div class='portal-button' data-name='" + key + "'>");
+            let portalImg = $("<img alt='image' class='portal-img'>")
             .attr("src", portals[key].imageURL);
             eachPortal.append(portalImg);
             $("#portalContainer").append(eachPortal);
         }
     }
 
+    //Update our currentScore
+    function updateCurrentScore(portal) {
+        currentScore += portals[portal.attr("data-name")].points;
+    }
+
+    //Display our currentScore on the page
+    function showCurrentScore () {
+        let scoreDiv = $("<div id='score'>").text(currentScore);
+        $("score-area").html();
+        $("score-area").html(scoreDiv);
+    }
+
+    //Game Start function calls
+    reset();
+    update();
+    showPortals();
+    showCurrentScore();
+
+    //Our portals are buttons so we must tie click events to them using a function
+    $(".portal-button").on("click", function(event) {
+        updateCurrentScore($(this));
+        showCurrentScore();
+
+        if (currentScore === 137) {
+            wins++;
+            reset();
+            update(true);
+        }
+
+        else if (currentScore > 137) {
+            losses++;
+            reset();
+            update(false);
+        }
+    });
 });
